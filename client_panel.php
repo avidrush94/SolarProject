7475107ec392b6a7aa346224080d8e465198435b
avidrush94 <avidrush94@gmail.com>
2017-04-26 22:51:20 +0530

    Removed all the charts and graphs
    Removed all the chart and graphs to make the interface a lot cleaner.

--- client_panel.php
+++ client_panel.php
@@ -21,32 +21,6 @@ if ($loginResult->num_rows == 1) {
   <link rel="stylesheet" href="resources/ss-css.css">
   <link rel="stylesheet" href="resources/ss-theme-css.css">
   <script src="https://use.fontawesome.com/04dafedc6e.js"></script>
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-  <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Voltage', 'Current'],
-          ['10am',  1000,      400],
-          ['11am',  1170,      460],
-          ['12pm',  660,      1120],
-          ['1pm',  1030,      540]
-        ]);
-
-        var options = {
-          title: 'Company Performance',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-        chart.draw(data, options);
-      }
-    </script>
 
     <!--Script to load MAP-->
     <script>
@@ -72,35 +46,6 @@ if ($loginResult->num_rows == 1) {
       var url = "/map.php?latitude="+latitude+"&longitude="+longitude;
       document.getElementById('mapFrame').src = url;
     }
-    </script>
-
-
-    <!--Pie Chart data generation system-->
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
-        ]);
-
-        var options = {
-          title: 'My Daily Activities',
-          legend: 'none',
-          pieHole: 0.4,
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
     </script>
 
 
@@ -218,16 +163,6 @@ if ($loginResult->num_rows == 1) {
 
   </div>-->
 
-  <!--Voltage Current Line Graph-->
-  <div class="w3-container">
-    <div class="w3-threequarter">
-      <div id="curve_chart" style="height: 400px"></div>
-    </div>
-
-    <div class="w3-quarter">
-      <iframe id="mapFrame" style="height: 400px; border:0px; margin:0px; padding:0px;" alt="Loading MAP"></iframe>
-    </div>
-  </div>
 
   <!-- Footer -->
   <footer class=" w3-theme-dark w3-padding-16 w3-margin-top">
